a9d1afbc91c14480525a2c20c5580cb45bec382e
HQVinh <HQVinh@users.noreply.github.com>
2022-06-17 23:13:36 -0400

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { addExpense, editExpense, removeExpense } from './actions/expenses';
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css'
-import './styles/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense, editExpense, removeExpense } from "./actions/expenses";
+import {
+  setTextFilter,
+  sortByDate,
+  sortByAmount,
+  setStartDate,
+  setEndDate
+} from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
 const store = configureStore();
 
@@ -16,11 +22,15 @@ const store = configureStore();
 // setTextFilter -> bill
 // getVisibleExpenses -> print visible ones to screen
 
-const expenseOne = store.dispatch(addExpense({ description: 'Water bill', amount: 1000}));
-const expenseTwo = store.dispatch(addExpense({ description: 'Gas bill', amount: 5000, createdAt: 1000}));
-const expenseThree = store.dispatch(addExpense({ description: 'Turtur', amount: 10000000}));
-
-
+const expenseOne = store.dispatch(
+  addExpense({ description: "Water bill", amount: 1000 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: "Gas bill", amount: 5000, createdAt: 1000 })
+);
+const expenseThree = store.dispatch(
+  addExpense({ description: "Turtur", amount: 10000000 })
+);
 
 const state = store.getState();
 
@@ -32,4 +42,4 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById("app"));
